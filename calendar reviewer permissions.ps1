b9e804b505d325edf0dea16fa7cdfd1017fe44ca
slanetw <39952297+slanetw@users.noreply.github.com>
2018-06-06 15:44:36 +0100

    Update calendar reviewer permissions.ps1
    Removed live OU Structure

--- calendar reviewer permissions.ps1
+++ calendar reviewer permissions.ps1
@@ -2,10 +2,10 @@
 
 ## Test the policy change with the following script ##
 
-##  Variables - SearchBase  This is the DN of the OU Path which needs to be changed for each OU that we are running this against
+##  Variables - SearchBase  This is the DN of the OU Path which needs to be changed for each OU that we are running this against ##
 ##              Access Rights - This is what we are providing the permission for (Default is AvailabilityOnly) ##
 
-$ouusers = Get-ADUser -Filter * -SearchBase “OU=Training,OU=Users,OU=London,OU=Resources,DC=law,DC=firm,DC=priv”
+$ouusers = Get-ADUser -Filter * -SearchBase “OU=**,OU=**,OU=**,OU=**,DC=**,DC=**,DC=**”
 Foreach ($Mailbox in $ouusers)
 {
 $path = $Mailbox.userprincipalname + “:\” + (Get-MailboxFolderStatistics $Mailbox.userprincipalname | Where-Object { $_.Foldertype -eq “Calendar” } | Select-Object -First 1).Name
@@ -14,21 +14,21 @@ Set-mailboxfolderpermission -identity ($path) -user Default -Accessrights review
 
 ## Live script is below and configured ##
 
-$ouusers = Get-ADUser -Filter * -SearchBase “OU=Fee Earning Departments,OU=Users,OU=London,OU=Resources,DC=law,DC=firm,DC=priv”
+$ouusers = Get-ADUser -Filter * -SearchBase “OU=**,OU=**,OU=**,OU=**,DC=**,DC=**,DC=**”
 Foreach ($Mailbox in $ouusers)
 {
 $path = $Mailbox.userprincipalname + “:\” + (Get-MailboxFolderStatistics $Mailbox.userprincipalname | Where-Object { $_.Foldertype -eq “Calendar” } | Select-Object -First 1).Name
 Set-mailboxfolderpermission -identity ($path) -user Default -Accessrights reviewer
 }
 
-$ouusers = Get-ADUser -Filter * -SearchBase “OU=Support Departments,OU=Users,OU=London,OU=Resources,DC=law,DC=firm,DC=priv”
+$ouusers = Get-ADUser -Filter * -SearchBase “OU=**,OU=**,OU=**,OU=**,DC=**,DC=**,DC=**”
 Foreach ($Mailbox in $ouusers)
 {
 $path = $Mailbox.userprincipalname + “:\” + (Get-MailboxFolderStatistics $Mailbox.userprincipalname | Where-Object { $_.Foldertype -eq “Calendar” } | Select-Object -First 1).Name
 Set-mailboxfolderpermission -identity ($path) -user Default -Accessrights reviewer
 }
 
-$ouusers = Get-ADUser -Filter * -SearchBase “OU=Secondees,OU=Users,OU=London,OU=Resources,DC=law,DC=firm,DC=priv”
+$ouusers = Get-ADUser -Filter * -SearchBase “OU=**,OU=**,OU=**,OU=**,DC=**,DC=**,DC=**”
 Foreach ($Mailbox in $ouusers)
 {
 $path = $Mailbox.userprincipalname + “:\” + (Get-MailboxFolderStatistics $Mailbox.userprincipalname | Where-Object { $_.Foldertype -eq “Calendar” } | Select-Object -First 1).Name
@@ -36,7 +36,7 @@ Set-mailboxfolderpermission -identity ($path) -user Default -Accessrights review
 }
 
 
-$ouusers = Get-ADUser -Filter * -SearchBase “OU=Fee Earning Departments,OU=Users,OU=London,OU=Resources,DC=law,DC=firm,DC=priv”
+$ouusers = Get-ADUser -Filter * -SearchBase “OU=**,OU=**,OU=**,OU=**,DC=**,DC=**,DC=**”
 Foreach ($Mailbox in $ouusers)
 {
 $path = $Mailbox.userprincipalname + “:\” + (Get-MailboxFolderStatistics $Mailbox.userprincipalname | Where-Object { $_.Foldertype -eq “Calendar” } | Select-Object -First 1).Name
@@ -48,28 +48,28 @@ Get-mailboxfolderpermission -identity ($path) -user Default
 
 AvailabilityOnly
 
-$ouusers = Get-ADUser -Filter * -SearchBase “OU=Training,OU=Users,OU=London,OU=Resources,DC=law,DC=firm,DC=priv”
+$ouusers = Get-ADUser -Filter * -SearchBase “OU=**,OU=**,OU=**,OU=**,DC=**,DC=**,DC=**”
 Foreach ($Mailbox in $ouusers)
 {
 $path = $Mailbox.userprincipalname + “:\” + (Get-MailboxFolderStatistics $Mailbox.userprincipalname | Where-Object { $_.Foldertype -eq “Calendar” } | Select-Object -First 1).Name
 Set-mailboxfolderpermission -identity ($path) -user Default -Accessrights AvailabilityOnly
 }
 
-$ouusers = Get-ADUser -Filter * -SearchBase “OU=Fee Earning Departments,OU=Users,OU=London,OU=Resources,DC=law,DC=firm,DC=priv”
+$ouusers = Get-ADUser -Filter * -SearchBase “OU=**,OU=**,OU=**,OU=**,DC=**,DC=**,DC=**”
 Foreach ($Mailbox in $ouusers)
 {
 $path = $Mailbox.userprincipalname + “:\” + (Get-MailboxFolderStatistics $Mailbox.userprincipalname | Where-Object { $_.Foldertype -eq “Calendar” } | Select-Object -First 1).Name
 Set-mailboxfolderpermission -identity ($path) -user Default -Accessrights AvailabilityOnly
 }
 
-$ouusers = Get-ADUser -Filter * -SearchBase “OU=Support Departments,OU=Users,OU=London,OU=Resources,DC=law,DC=firm,DC=priv”
+$ouusers = Get-ADUser -Filter * -SearchBase “OU=**,OU=**,OU=**,OU=**,DC=**,DC=**,DC=**”
 Foreach ($Mailbox in $ouusers)
 {
 $path = $Mailbox.userprincipalname + “:\” + (Get-MailboxFolderStatistics $Mailbox.userprincipalname | Where-Object { $_.Foldertype -eq “Calendar” } | Select-Object -First 1).Name
 Set-mailboxfolderpermission -identity ($path) -user Default -Accessrights AvailabilityOnly
 }
 
-$ouusers = Get-ADUser -Filter * -SearchBase “OU=Secondees,OU=Users,OU=London,OU=Resources,DC=law,DC=firm,DC=priv”
+$ouusers = Get-ADUser -Filter * -SearchBase “OU=**,OU=**,OU=**,OU=**,DC=**,DC=**,DC=**”
 Foreach ($Mailbox in $ouusers)
 {
 $path = $Mailbox.userprincipalname + “:\” + (Get-MailboxFolderStatistics $Mailbox.userprincipalname | Where-Object { $_.Foldertype -eq “Calendar” } | Select-Object -First 1).Name
